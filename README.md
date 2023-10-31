4a342f0d73e2e2f9ee0c075a5fd990f9b72222e1
Kevins Markuss Kanalis <KevinsKanalis@gmail.com>
2023-10-31 22:33:04 +0200

    Almost finished but broken and I quit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # rust password generator
 Tts a rust password generator this project is the same as my cpp password generator but in rust
 I only made it to learn rust
+Not working, it broken and I give up tbh.
--- src/main.rs
+++ src/main.rs
@@ -44,14 +44,14 @@ fn write_to_file(input_string:String) -> std::io::Result<()> {
 }
 
 fn main() {
-    let mut how_many: i32 = 0;
-    let mut how_long: i32 = 0;
+    let mut how_many_string: String = String::from(" ");
+    let mut how_long_string: String = String::from(" ");
     let mut spec_cherecter_bool: bool = false;
     let mut external_bool: bool = false;
     let mut spec_cherecter: String = String::from(" ");
     let mut external: String = String::from(" ");
 
-    println!("\n Save it in a file or generate in cosole? (Yes for console):");
+    println!("\n Save it in a file or generate in cosole? (No for console):");
     let _ = std::io::stdin().read_line(&mut external);
 
     if normalize(external.clone()) == String::from("yes") || normalize(external.clone()) == String::from("y"){
@@ -70,42 +70,81 @@ fn main() {
         spec_cherecter_bool = false;
     }
 
-    // println!("\n how many passwords?(int):");
-    // let _ = std::io::stdin().read_line(&mut how_many);
+    println!("\n how many passwords?(int):");
+    let _ = std::io::stdin().read_line(&mut how_many_string);
+    let how_many: i32 = how_many_string.trim().parse().unwrap(); //form string to int
 
-    // println!("\n how long each passwords?(int):");
-    // let _ = std::io::stdin().read_line(&mut how_long);  
+    println!("\n how long each passwords?(int):");
+    let _ = std::io::stdin().read_line(&mut how_long_string);
+    let how_long: i32 = how_long_string.trim().parse().unwrap(); //form string to int
 
-    
+    //chad :https://stackoverflow.com/questions/27043268/convert-a-string-to-int/54733404#54733404
+
     //main gen stuff
     let mut buffer:String = String::new();
+    let new_line:String = String::from("\n");
+
+    let mut how_long_clone:i32;
+    let mut how_many_clone:i32;
+
     if external_bool == true{
         if spec_cherecter_bool == true{
-            buffer = spec_char_gen();
+            how_many_clone = how_many;
+            while how_many_clone != 0 || how_many_clone < 0   {
+                how_long_clone = how_long;
+                while how_long_clone != 0 || how_long_clone < 0   {
+                    let mut temp:String = String::from(spec_char_gen());
+                    buffer = format!("{buffer}{temp}");
+                    how_long_clone = how_long_clone - 1;
+                }
+                buffer = format!("{buffer}{new_line}");
+                how_long_clone = how_long;
+                how_many_clone = how_many_clone - 1;
+            }
         }else{
-            non_spec_char_gen();
+            how_many_clone = how_many;
+            while how_many_clone != 0 || how_many_clone < 0   {
+                how_long_clone = how_long;
+                while how_long_clone != 0 || how_long_clone < 0   {
+                    let mut temp:String = String::from(non_spec_char_gen());
+                    buffer = format!("{buffer}{temp}");
+                    how_long_clone = how_long_clone - 1;
+                }
+                buffer = format!("{buffer}{new_line}");
+                how_long_clone = how_long;
+                how_many_clone = how_many_clone - 1;
+            }
         }
         let _ = write_to_file(buffer);
-
     }else{
         if spec_cherecter_bool == true{
-            while how_many != 0   {
-                while how_long != 0   {
-                    spec_char_gen();
-                    how_long = how_long - 1;
+            how_many_clone = how_many;
+            while how_many_clone != 0 || how_many_clone < 0   {
+                how_long_clone = how_long;
+                while how_long_clone != 0 || how_long_clone < 0   {
+                    let mut temp:String = String::from(spec_char_gen());
+                    buffer = format!("{buffer}{temp}");
+                    how_long_clone = how_long_clone - 1;
                 }
-                how_many = how_many - 1;
+                println!("{}",buffer);
+                buffer = String::new();
+                how_long_clone = how_long;
+                how_many_clone = how_many_clone - 1;
             }
-
         }else{
-            while how_many != 0   {
-                while how_long != 0   {
-                    non_spec_char_gen();
-                    how_long = how_long - 1;
+            how_many_clone = how_many;
+            while how_many_clone != 0 || how_many_clone < 0   {
+                how_long_clone = how_long;
+                while how_long_clone != 0 || how_long_clone < 0   {
+                    let mut temp:String = String::from(non_spec_char_gen());
+                    buffer = format!("{buffer}{temp}");
+                    how_long_clone = how_long_clone - 1;
                 }
-                how_many = how_many - 1;
+                println!("{}",buffer);
+                buffer = String::new();
+                how_long_clone = how_long;
+                how_many_clone = how_many_clone - 1;
             }
-
         }
     }
 
